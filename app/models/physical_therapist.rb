0f74b9fc907d2a84ad3fac56bd0bc44e618f5365
Christian <cedenoc181@gmail.com>
2023-02-01 22:07:24 -0500

    models and serializer

--- app/models/physical_therapist.rb
+++ app/models/physical_therapist.rb
@@ -1,2 +1,7 @@
 class PhysicalTherapist < ApplicationRecord
+    has_many :appointments, dependent: :destroy
+    has_many :users, through: :appointments 
+    has_many :reviews, dependent: :destroy
+    has_many :users, through: :reviews
+
 end
