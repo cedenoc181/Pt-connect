c47e2a5d5377a5a32477dfe7f4a755b0e8b3defc
Christian <cedenoc181@gmail.com>
2023-02-03 20:06:46 -0500

    auth controller added

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,32 @@
 class AuthController < ApplicationController
+    # skip_before_action :authorized, only: [:login]
+    skip_before_action :authorized
+
+rescue_from ActiveRecord::RecordNotFound, with: :invalid_username
+# if user render user else if pt render pt ?
+    def login 
+        @user = User.find_by!(username: login_params[:username])
+        if @user.authenticate(login_params[:password])
+            @token = encode_token(user_id: @user.id)
+            render json: {
+                user: UserSerializer.new(@user),
+                token: @token
+            }, status: :accepted
+        else
+            render json: {message: 'Incorrect password'}, status: :unauthorized
+        end
+    end
+
+
+    private 
+
+    def login_params
+        params.permit(:username, :password)
+    end
+
+    def invalid_username 
+        render json: {message: 'Invalid username'}, status: :unauthorized
+    end
 end
+
+
