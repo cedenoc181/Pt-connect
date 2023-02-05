b6c45f3fb0d401646302f1dcad1f78637b5b0bd0
Christian <cedenoc181@gmail.com>
2023-02-05 15:56:25 -0500

    user controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  
-  resources :reviews
-  resources :physical_therapists
-  resources :favorites
-  resources :appointments
-  resources :exercises
-  resources :users
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # route to favorite exercise 
+  # get "favorites/user/#{:user_id}" => 'favorites#user_favorites'
+  post "/auth/login", to: "auth#login"
+  get "/fav", to: "users#favorite_exercises"
+  delete "/fav/:exercise_id", to: "users#fav_delete"
+  get "/my-appointments", to: "users#get_appointments"
+  resources :favorites, only: [:index, :show, :create, :destroy]
+  resources :exercises, only:[:index, :show]
+  resources :reviews, only:[:index, :show, :create, :destroy]
+  resources :appointments, only:[:index, :show, :create, :destroy]
+  resources :physical_therapists, only:[:index, :show, :create, :update, :destroy]
+  resources :users, only:[:index, :show, :update, :create, :destroy]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
